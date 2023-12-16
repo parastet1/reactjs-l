50dce65b13b6daa87e8d73812fbcad5c372e309f
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-16 21:32:33 +0530

    updated files: react essentials better way of outputting images

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -7,7 +9,7 @@ function getRandomInt(max) {
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescriptions[getRandomInt(2)]} React concepts you will need for
