0a2b1c60735c5faa034ae61f8290ef035596e349
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-17 16:38:58 +0530

    updated files: react essentials component composition using children prop

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
@@ -27,6 +28,15 @@ function App() {
             />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
