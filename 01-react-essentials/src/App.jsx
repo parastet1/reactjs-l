c01e6639b11d4d21a4fd01197f74c84ec783e9eb
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-17 19:46:14 +0530

    updated files: react essentials reacting to events

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -4,6 +4,10 @@ import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
+  function handleSelect() {
+    console.log("selected button");
+  }
+
   return (
     <div>
       <Header />
@@ -32,11 +36,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
+          <div id="tab-content"></div>
         </section>
       </main>
     </div>
