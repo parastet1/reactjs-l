e57a8e5525b2ede94c7718fdf6d719e4a2154927
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-17 09:03:42 +0530

    updated files: react essentials code organization best practices

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -1,45 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[getRandomInt(2)]} React concepts you will need for
-        almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-// function CoreConcept(props) {
-//   return (
-//     <li>
-//       <img src={props.image} alt={props.title} />
-//       <h3>{props.title}</h3>
-//       <p>{props.description}</p>
-//     </li>
-//   );
-// }
-
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
---
+++ 01-react-essentials/src/components/CoreConcept.jsx
@@ -0,0 +1,21 @@
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+
+function CoreConcept({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+export default CoreConcept;
