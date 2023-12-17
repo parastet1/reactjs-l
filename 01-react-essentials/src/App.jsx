340af9f82e148abeaaa3e83692e7edd76c33cdbf
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-17 19:56:05 +0530

    updated files: react essentials passing function args in event

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -4,8 +4,8 @@ import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
-  function handleSelect() {
-    console.log("selected button");
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
   }
 
   return (
@@ -36,10 +36,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("Components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
           </menu>
           <div id="tab-content"></div>
         </section>
