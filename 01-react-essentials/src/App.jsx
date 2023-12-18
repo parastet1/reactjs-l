fd5de8ecb61f7fe3a32d1df8f8236d310bcf0a7a
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-18 21:59:40 +0530

    updated files: react essentials adding styles dynamically

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -7,6 +7,18 @@ import { CORE_CONCEPTS, EXAMPLES } from "./data";
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
 
+  let tabContent = <p>Please select a topic</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <section id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>{EXAMPLES[selectedTopic].code}</pre>
+      </section>
+    );
+  }
+
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
@@ -39,22 +51,33 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
         </section>
-        {!selectedTopic && <p>Please select a topic</p>}
-        {selectedTopic && (
-          <section id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>{EXAMPLES[selectedTopic].code}</pre>
-          </section>
-        )}
+        {tabContent}
       </main>
     </div>
   );
