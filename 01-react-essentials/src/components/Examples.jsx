741a653c7a29fae72e2c5bac672ba5460c3a0530
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-21 21:35:39 +0530

    updated files: react essentials setting element type dynamically

--- 01-react-essentials/src/components/Examples.jsx
+++ 01-react-essentials/src/components/Examples.jsx
@@ -26,6 +26,7 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples">
       <Tabs
+        buttonsContainer="menu"
         buttons={
           <>
             <TabButton
